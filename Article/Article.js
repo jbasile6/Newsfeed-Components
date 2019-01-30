7ea8ce4bd7a86154fe3f1403eca21ed2ec6959ff
James Basile <jbasile14@gmail.com>
2019-01-30 16:41:47 -0500

    text content for 'Expand Buttons' added

--- Article/Article.js
+++ Article/Article.js
@@ -1,14 +1,18 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.article = article;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton = article.querySelector('.expandButton');
+    // Using your expandButton reference, update the text on your
+    // expandButton to say "expand"
+    this.expandButton.textContent = 'Expand';
     
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    // Set a click handler on the expandButton reference, 
+    //calling the expandArticle method.
     
   }
 
@@ -22,8 +26,15 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+- With your selection in place, now chain .forEach()
+    on to the articles variable to iterate over the articles
+    NodeList and create a new instance of Article by passing in each
+    article as a parameter to the constructor.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+//pass each article as a param to constructor
+articles.forEach(article => new Article(article));
+//see top of file ^^^^
